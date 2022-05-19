747d89c3936249ed4605eae66a0847b074cd568e
Kirsten <kmalcaba@gmail.com>
2022-05-19 14:32:21 -0700

    Add event listeners for numbers and submit button

---
+++ interactive-rating-component/js/script.js
@@ -0,0 +1,23 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const ratingBtns = document.querySelectorAll(".number");
+  ratingBtns.forEach((element) =>
+    element.addEventListener("click", selectRating)
+  );
+
+  const submitBtn = document.querySelector("button.submit");
+  submitBtn.addEventListener("click", showThankYouBox);
+});
+
+const showThankYouBox = () => {
+  const thankYouBox = document.querySelector(".thank-you");
+  const rateBox = document.querySelector(".rate-box");
+  rateBox.style.display = "none";
+  thankYouBox.style.display = "flex";
+};
+
+const selectRating = (e) => {
+  const number = e.target.textContent;
+  e.target.class.toggle("active");
+};
+
+const updateSelected = (number) => {};
