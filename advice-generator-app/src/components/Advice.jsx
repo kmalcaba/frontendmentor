fd681982f115526e82fdd361e5cb2d9c2b9b2452
Kirsten <kmalcaba@gmail.com>
2022-05-23 19:45:27 -0700

    Add Advice component

---
+++ advice-generator-app/src/components/Advice.jsx
@@ -0,0 +1,12 @@
+import "../css/Advice.css";
+
+const Advice = () => {
+  return (
+    <div className='Advice'>
+      "It is easy to sit up and take notice, what's difficult is getting up and
+      taking action."
+    </div>
+  );
+};
+
+export default Advice;
--- advice-generator-app/src/components/AdviceBox.jsx
+++ advice-generator-app/src/components/AdviceBox.jsx
@@ -1,10 +1,12 @@
 import "../css/AdviceBox.css";
 import AdviceNumber from "./AdviceNumber";
+import Advice from "./Advice";
 
 const AdviceBox = () => {
   return (
     <div className='AdviceBox'>
       <AdviceNumber />
+      <Advice />
     </div>
   );
 };
