7a3ffcab36137fdec42d5cd8a58c76094689020d
Kirsten <kmalcaba@gmail.com>
2022-06-04 13:27:16 -0700

    Change from <div> to <button>

--- advice-generator-app/src/components/Dice.jsx
+++ advice-generator-app/src/components/Dice.jsx
@@ -3,9 +3,9 @@ import { ReactComponent as DiceSvg } from "../img/icon-dice.svg";
 
 const Dice = ({ handleClick }) => {
   return (
-    <div className='Dice' onClick={handleClick}>
+    <button className='Dice' onClick={handleClick}>
       <DiceSvg />
-    </div>
+    </button>
   );
 };
 
