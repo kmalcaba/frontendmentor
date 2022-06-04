550840fb511ef108f84710f39f01b4a735ce5994
Kirsten <kmalcaba@gmail.com>
2022-06-04 14:10:56 -0700

    Create separate component for Divider

---
+++ advice-generator-app/src/components/Divider.jsx
@@ -0,0 +1,9 @@
+import { ReactComponent as DividerDesktop } from "../img/pattern-divider-desktop.svg";
+import { ReactComponent as DividerMobile } from "../img/pattern-divider-mobile.svg";
+import "../css/Divider.css";
+
+const Divider = () => {
+  return <div className='Divider'></div>;
+};
+
+export default Divider;
