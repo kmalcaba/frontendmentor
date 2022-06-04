30b646c96c2e2bcadbf3ebef901dc8f2dcb0ac28
Kirsten <kmalcaba@gmail.com>
2022-06-03 17:25:32 -0700

    Replace double quotes

--- advice-generator-app/src/components/Advice.jsx
+++ advice-generator-app/src/components/Advice.jsx
@@ -3,8 +3,8 @@ import "../css/Advice.css";
 const Advice = () => {
   return (
     <div className='Advice'>
-      "It is easy to sit up and take notice, what's difficult is getting up and
-      taking action."
+      “It is easy to sit up and take notice, what's difficult is getting up and
+      taking action.”
     </div>
   );
 };
