2f4fc6b1048eb3965f4257c0affa24733a99ae9e
Kirsten <kmalcaba@gmail.com>
2022-06-03 17:46:06 -0700

    Add Dice component

--- advice-generator-app/src/components/AdviceBox.jsx
+++ advice-generator-app/src/components/AdviceBox.jsx
@@ -1,6 +1,7 @@
 import "../css/AdviceBox.css";
 import AdviceNumber from "./AdviceNumber";
 import Advice from "./Advice";
+import Dice from "./Dice";
 import { ReactComponent as Divider } from "../img/pattern-divider-desktop.svg";
 import { ReactComponent as DividerMobile } from "../img/pattern-divider-mobile.svg";
 
@@ -10,6 +11,7 @@ const AdviceBox = () => {
       <AdviceNumber />
       <Advice />
       <Divider />
+      <Dice />
     </div>
   );
 };
---
+++ advice-generator-app/src/components/Dice.jsx
@@ -0,0 +1,12 @@
+import "../css/Dice.css";
+import { ReactComponent as DiceSvg } from "../img/icon-dice.svg";
+
+const Dice = () => {
+  return (
+    <div className='Dice'>
+      <DiceSvg />
+    </div>
+  );
+};
+
+export default Dice;
