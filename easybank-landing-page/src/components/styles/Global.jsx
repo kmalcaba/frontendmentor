a6550a9faa32a3ccd8998d2e48e26c52759c263b
Kirsten Malcaba <kmalcaba@gmail.com>
2022-06-13 12:10:09 -0700

    Update default font size

--- easybank-landing-page/src/components/styles/Global.jsx
+++ easybank-landing-page/src/components/styles/Global.jsx
@@ -3,7 +3,7 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyles = createGlobalStyle`
   * {
     box-sizing: border-box;
-    font-size: 16px;
+    font-size: 18px;
   }
 
 `;
