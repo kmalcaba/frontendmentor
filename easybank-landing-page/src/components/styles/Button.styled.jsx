729100cc0d37546bdb1d510f3da783f923a00371
Kirsten Malcaba <kmalcaba@gmail.com>
2022-06-13 11:53:50 -0700

    Update basic button styles

---
+++ easybank-landing-page/src/components/styles/Button.styled.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  border: none;
+  outline: none;
+  padding: 15px 40px;
+  border-radius: 50px;
+  cursor: pointer;
+  font-weight: 600;
+`;
