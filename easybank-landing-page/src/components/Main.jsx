70afdc06208c23f39214d83481b427effa987119
Kirsten Malcaba <kmalcaba@gmail.com>
2022-06-13 15:36:34 -0700

    Extract into new component

--- easybank-landing-page/src/components/Main.jsx
+++ easybank-landing-page/src/components/Main.jsx
@@ -1,31 +1,10 @@
-import {
-  StyledMain,
-  IntroContainer,
-  Intro,
-  Title,
-  Paragraph,
-  Image,
-  BgContainer,
-} from "./styles/Main.styled";
-import { Button } from "./styles/Button.styled";
+import { StyledMain } from "./styles/Main.styled";
+import IntroContainer from "./IntroContainer";
 
 const Main = () => {
   return (
     <StyledMain>
-      <IntroContainer>
-        <Intro>
-          <Title>Next generation digital banking</Title>
-          <Paragraph>
-            Take your financial life online. Your Easybank account will be a
-            one-stop-shop for spending, saving, budgeting, investing, and much
-            more.
-          </Paragraph>
-          <Button>Request Invite</Button>
-        </Intro>
-        <BgContainer>
-          <Image src='./images/image-mockups.png' alt='' />
-        </BgContainer>
-      </IntroContainer>
+      <IntroContainer />
     </StyledMain>
   );
 };
--- easybank-landing-page/src/components/styles/Main.styled.jsx
+++ easybank-landing-page/src/components/styles/Main.styled.jsx
@@ -7,19 +7,6 @@ export const StyledMain = styled.main`
   width: 100%;
 `;
 
-export const IntroContainer = styled(MainContainer)`
-  display: flex;
-  height: 655px;
-  padding-right: 0;
-  justify-content: space-around;
-`;
-
-export const Intro = styled.div`
-  flex: 1 0 auto;
-  max-width: 460px;
-  margin-right: 100px;
-`;
-
 export const Title = styled.h1`
   margin-top: 180px;
   font-weight: 300;
@@ -31,18 +18,4 @@ export const Paragraph = styled.p`
   color: ${({ theme }) => theme.colors.text_secondary};
   line-height: 1.5rem;
   margin: 30px 0;
-`;
-
-export const BgContainer = styled.div`
-  position: relative;
-  background-image: url(./images/bg-intro-desktop.svg);
-  background-repeat: no-repeat;
-  background-position: 10% 50%;
-  flex: 1;
-`;
-
-export const Image = styled.img`
-  transform: translateY(-12%) translateX(18%);
-  position: absolute;
-  right: 0;
 `;
