49595dadcc923002f3caa802cbae4dba7459ab59
Kirsten Malcaba <kmalcaba@gmail.com>
2022-06-13 13:58:36 -0700

    Change width and padding

--- easybank-landing-page/src/components/styles/MainContainer.styled.jsx
+++ easybank-landing-page/src/components/styles/MainContainer.styled.jsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const MainContainer = styled.div`
-  width: 1120px;
+  width: 1520px;
   max-width: 100%;
-  padding: 0 20px;
+  padding: 0 80px;
   margin: 0 auto;
 `;
