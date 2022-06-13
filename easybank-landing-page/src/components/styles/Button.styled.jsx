9cb48d7b9139a52cff174e5e8f03c185c72abfa7
Kirsten Malcaba <kmalcaba@gmail.com>
2022-06-13 12:16:00 -0700

    Add background gradient

--- easybank-landing-page/src/components/styles/Button.styled.jsx
+++ easybank-landing-page/src/components/styles/Button.styled.jsx
@@ -7,4 +7,10 @@ export const Button = styled.button`
   border-radius: 50px;
   cursor: pointer;
   font-weight: 600;
+  background: linear-gradient(
+    90deg,
+    ${({ theme }) => theme.colors.accent_primary} 0%,
+    ${({ theme }) => theme.colors.accent_secondary} 100%
+  );
+  color: ${({ theme }) => theme.colors.white};
 `;
