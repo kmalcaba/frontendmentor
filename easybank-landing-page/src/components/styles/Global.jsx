1812e0bd0e1fc15951cc703fcfd0757905451155
Kirsten Malcaba <kmalcaba@gmail.com>
2022-06-13 13:06:06 -0700

    Add default font family and size

--- easybank-landing-page/src/components/styles/Global.jsx
+++ easybank-landing-page/src/components/styles/Global.jsx
@@ -3,9 +3,13 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyles = createGlobalStyle`
   * {
     box-sizing: border-box;
-    font-size: 18px;
   }
-
+  :root {
+    font-family: 'Public Sans', sans-serif;
+    font-size: 18px;
+    margin: 0;
+    padding: 0;
+  }
 `;
 
 export default GlobalStyles;
