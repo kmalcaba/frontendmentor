d4a84560ff40a7fcb5ebd852e1662ff7cb15ad1a
Kirsten Malcaba <kmalcaba@gmail.com>
2022-06-13 13:44:37 -0700

    Add hover styles

--- easybank-landing-page/src/components/styles/Header.styled.jsx
+++ easybank-landing-page/src/components/styles/Header.styled.jsx
@@ -3,6 +3,7 @@ import { MainContainer } from "./MainContainer.styled";
 
 export const StyledHeader = styled.header`
   background-color: ${({ theme }) => theme.colors.white};
+  width: 100%;
 `;
 
 export const HeaderContainer = styled(MainContainer)`
@@ -19,9 +20,14 @@ export const NavGroup = styled.nav`
 
 export const NavButton = styled.li`
   list-style: none;
-  padding: 30px 15px;
+  padding: 27px 0 28px 0;
+  margin: 0 15px;
   cursor: pointer;
   color: ${({ theme }) => theme.colors.text_secondary};
-  font-family: "Public Sans", sans-serif;
+  border-bottom: 4px solid transparent;
+  transition: ease-in-out 0.2s;
   font-size: 0.8rem;
+  &:hover {
+    border-bottom: 4px solid ${({ theme }) => theme.colors.accent_primary};
+  }
 `;
