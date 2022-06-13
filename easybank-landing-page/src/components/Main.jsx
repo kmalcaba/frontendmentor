1e91422beec87def0df0fdc90a20da784eb3c4fd
Kirsten Malcaba <kmalcaba@gmail.com>
2022-06-13 16:03:18 -0700

    Add features container component

--- easybank-landing-page/src/components/Main.jsx
+++ easybank-landing-page/src/components/Main.jsx
@@ -1,10 +1,12 @@
 import { StyledMain } from "./styles/Main.styled";
 import IntroContainer from "./IntroContainer";
+import FeaturesContainer from "./FeaturesContainer";
 
 const Main = () => {
   return (
     <StyledMain>
       <IntroContainer />
+      <FeaturesContainer />
     </StyledMain>
   );
 };
