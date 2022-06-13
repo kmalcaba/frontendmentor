0306914fa4e0c52dbb45c8d4bc532764ec9a3909
Kirsten Malcaba <kmalcaba@gmail.com>
2022-06-13 10:44:31 -0700

    Build React App with styled-components

--- easybank-landing-page/src/App.js
+++ easybank-landing-page/src/App.js
@@ -1,15 +1,5 @@
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-
 function App() {
-  return (
-    <div className='App'>
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
