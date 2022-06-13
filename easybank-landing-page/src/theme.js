39a33881dc5cdd74470e911c60c1da8f40e6d758
Kirsten Malcaba <kmalcaba@gmail.com>
2022-06-13 12:10:20 -0700

    Add color palette

---
+++ easybank-landing-page/src/theme.js
@@ -0,0 +1,11 @@
+export const theme = {
+  colors: {
+    accent_primary: "hsl(136, 65%, 51%)",
+    accent_secondary: "hsl(192, 70%, 51%)",
+    text_primary: "hsl(233, 26%, 24%)",
+    text_secondary: "hsl(233, 8%, 62%)",
+    bg_primary: "hsl(0, 0%, 98%)",
+    bg_secondary: "hsl(220, 16%, 96%)",
+    white: "hsl(0, 0%, 100%)",
+  },
+};
