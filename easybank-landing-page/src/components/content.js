6e8d017f58a1f1aad6c45d9a27c4e2610141c06a
Kirsten Malcaba <kmalcaba@gmail.com>
2022-06-13 16:04:01 -0700

    Add content for cards

---
+++ easybank-landing-page/src/components/content.js
@@ -0,0 +1,28 @@
+const content = [
+  {
+    id: 0,
+    title: "Online Banking",
+    body: "Our modern web and mobile applications allow you to keep track of your finances wherever you are in the world.",
+    image: "icon-online.svg",
+  },
+  {
+    id: 1,
+    title: "Simple Budgeting",
+    body: "See exactly where your money goes each month. Receive notifications when you're close to hitting your limits.",
+    image: "icon-budgeting.svg",
+  },
+  {
+    id: 2,
+    title: "Fast Onboarding",
+    body: "We don't do branches. Open your account in minutes online and start taking control of your finances right away.",
+    image: "icon-onboarding.svg",
+  },
+  {
+    id: 3,
+    title: "Open API",
+    body: "Manage your savings, investments, pension, and much more from one account. Tracking your money has never been easier.",
+    image: "icon-api.svg",
+  },
+];
+
+export default content;
