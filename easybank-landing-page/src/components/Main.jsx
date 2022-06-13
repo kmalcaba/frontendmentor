bc9ad68e38ab8148bc8d6ed4207a63a5e3a91ec1
Kirsten Malcaba <kmalcaba@gmail.com>
2022-06-13 15:21:23 -0700

    Update intro component styles

--- easybank-landing-page/src/components/Main.jsx
+++ easybank-landing-page/src/components/Main.jsx
@@ -1,7 +1,31 @@
-import { StyledMain } from "./styles/Main.styled";
+import {
+  StyledMain,
+  IntroContainer,
+  Intro,
+  Title,
+  Paragraph,
+  Image,
+  BgContainer,
+} from "./styles/Main.styled";
 
 const Main = () => {
-  return <StyledMain></StyledMain>;
+  return (
+    <StyledMain>
+      <IntroContainer>
+        <Intro>
+          <Title>Next generation digital banking</Title>
+          <Paragraph>
+            Take your financial life online. Your Easybank account will be a
+            one-stop-shop for spending, saving, budgeting, investing, and much
+            more.
+          </Paragraph>
+        </Intro>
+        <BgContainer>
+          <Image src='./images/image-mockups.png' alt='' />
+        </BgContainer>
+      </IntroContainer>
+    </StyledMain>
+  );
 };
 
 export default Main;
--- easybank-landing-page/src/components/styles/Main.styled.jsx
+++ easybank-landing-page/src/components/styles/Main.styled.jsx
@@ -1,3 +1,47 @@
 import styled from "styled-components";
+import { MainContainer } from "./MainContainer.styled";
 
-export const StyledMain = styled.main``;
+export const StyledMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+export const IntroContainer = styled(MainContainer)`
+  display: flex;
+  height: 655px;
+  padding-right: 0;
+  justify-content: space-around;
+`;
+
+export const Intro = styled.div`
+  flex: 1 0 auto;
+  max-width: 460px;
+  margin-right: 100px;
+`;
+
+export const Title = styled.h1`
+  margin-top: 180px;
+  margin-bottom: 30px;
+  font-weight: 300;
+  font-size: 3rem;
+  color: ${({ theme }) => theme.colors.text_primary};
+`;
+
+export const Paragraph = styled.p`
+  color: ${({ theme }) => theme.colors.text_secondary};
+`;
+
+export const BgContainer = styled.div`
+  position: relative;
+  background-image: url(./images/bg-intro-desktop.svg);
+  background-repeat: no-repeat;
+  background-position: 0% 50%;
+  flex: 1;
+`;
+
+export const Image = styled.img`
+  transform: translateY(-12%) translateX(20%);
+  position: absolute;
+  right: 0;
+`;
