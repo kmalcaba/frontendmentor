575ec968734b22add17244ccdac4a2ff5995ddd8
Kirsten Malcaba <kmalcaba@gmail.com>
2022-06-14 14:56:36 -0700

    Adjust padding

--- easybank-landing-page/src/components/styles/FeaturesContainer.styled.jsx
+++ easybank-landing-page/src/components/styles/FeaturesContainer.styled.jsx
@@ -5,7 +5,7 @@ export const StyledFeaturesContainer = styled.div`
   display: flex;
   /* height: 670px; */
   background-color: ${({ theme }) => theme.colors.bg_secondary};
-  padding: 105px 0;
+  padding: 105px 0 75px 0;
 `;
 
 export const Features = styled(MainContainer)``;
