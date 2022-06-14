bde18513e4e58cac96361e81f4c627d8be32c27d
Kirsten Malcaba <kmalcaba@gmail.com>
2022-06-14 16:19:27 -0700

    Add blog card component

--- easybank-landing-page/src/components/BlogContainer.jsx
+++ easybank-landing-page/src/components/BlogContainer.jsx
@@ -1,7 +1,23 @@
 import { StyledBlogContainer } from "./styles/BlogContainer.styled";
+import { FlexContainer } from "./styles/FlexContainer.styled";
+import { Heading } from "./styles/Main.styled";
+import { MainContainer } from "./styles/MainContainer.styled";
+import blogContent from "../blogcontent";
+import BlogCard from "./BlogCard";
 
 const BlogContainer = () => {
-  return <StyledBlogContainer>BlogContainer</StyledBlogContainer>;
+  return (
+    <StyledBlogContainer>
+      <MainContainer>
+        <Heading>Latest Articles</Heading>
+        <FlexContainer>
+          {blogContent.map((item) => (
+            <BlogCard key={item.id} item={item} />
+          ))}
+        </FlexContainer>
+      </MainContainer>
+    </StyledBlogContainer>
+  );
 };
 
 export default BlogContainer;
--- easybank-landing-page/src/components/styles/BlogContainer.styled.jsx
+++ easybank-landing-page/src/components/styles/BlogContainer.styled.jsx
@@ -1,3 +1,6 @@
 import styled from "styled-components";
 
-export const StyledBlogContainer = styled.div``;
+export const StyledBlogContainer = styled.div`
+  padding: 90px 0 80px 0;
+  background-color: ${({ theme }) => theme.colors.bg_primary};
+`;
