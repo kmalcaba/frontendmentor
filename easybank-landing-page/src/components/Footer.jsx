3954ef295a89c92db7479706fc78dcbbc1a3f9a1
Kirsten Malcaba <kmalcaba@gmail.com>
2022-06-14 16:52:27 -0700

    Add skeleton layout and basic styling

--- easybank-landing-page/src/components/Footer.jsx
+++ easybank-landing-page/src/components/Footer.jsx
@@ -1,7 +1,26 @@
+import { FlexContainer } from "./styles/FlexContainer.styled";
 import { StyledFooter } from "./styles/Footer.styled";
+import { MainContainer } from "./styles/MainContainer.styled";
+import { Button } from "./styles/Button.styled";
 
 const Footer = () => {
-  return <StyledFooter></StyledFooter>;
+  return (
+    <StyledFooter>
+      <MainContainer>
+        <FlexContainer>
+          <FlexContainer direction='column'>
+            {/* logo and social icons, space-around */}
+          </FlexContainer>
+          <FlexContainer>{/* links, row wrap */}</FlexContainer>
+          <FlexContainer direction='column'>
+            <Button>Request Invite</Button>
+            <p>&copy; Easybank. All Rights Reserved.</p>
+            {/* cta button  and copyright, space-around */}
+          </FlexContainer>
+        </FlexContainer>
+      </MainContainer>
+    </StyledFooter>
+  );
 };
 
 export default Footer;
--- easybank-landing-page/src/components/styles/Footer.styled.jsx
+++ easybank-landing-page/src/components/styles/Footer.styled.jsx
@@ -1,3 +1,6 @@
 import styled from "styled-components";
 
-export const StyledFooter = styled.footer``;
+export const StyledFooter = styled.footer`
+  background-color: ${({ theme }) => theme.colors.text_primary};
+  color: ${({ theme }) => theme.colors.white};
+`;
