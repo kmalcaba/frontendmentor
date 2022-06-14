986014b1c41d07ab8e328fd48a582028670309f0
Kirsten Malcaba <kmalcaba@gmail.com>
2022-06-14 15:43:46 -0700

    Add blog content

---
+++ easybank-landing-page/src/blogcontent.js
@@ -0,0 +1,26 @@
+const blogContent = [
+  {
+    id: 0,
+    title: "Receive money in any currency with no fees",
+    author: "Claire Robinson",
+    body: "The world is getting smaller and we're becoming more mobile. So why should you be forced to only receive money in a single",
+  },
+  {
+    id: 1,
+    title: "Treat yourself without worrying about money",
+    author: "Wilson Hutton",
+    body: "Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you",
+  },
+  {
+    id: 2,
+    title: "Take your Easybank card wherever you go",
+    author: "Wilson Hutton",
+    body: "We want you to enjoy your travels. This is why we don't charge any fees on purchases while you're abroad. We'll even show you",
+  },
+  {
+    id: 3,
+    title: "Our invite-only Beta accounts are now live!",
+    author: "Claire Robinson",
+    body: "After a lot of hard work by the whole team, we're excited to launch our closed beta. It's easy to request an invite through the site",
+  },
+];
