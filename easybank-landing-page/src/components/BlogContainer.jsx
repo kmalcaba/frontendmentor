bf88703b3c1b1a07a9d501fff837590aee46bff5
Kirsten Malcaba <kmalcaba@gmail.com>
2022-06-14 15:46:08 -0700

    Add blog container component

---
+++ easybank-landing-page/src/components/BlogContainer.jsx
@@ -0,0 +1,7 @@
+import { StyledBlogContainer } from "./styles/BlogContainer.styled";
+
+const BlogContainer = () => {
+  return <StyledBlogContainer>BlogContainer</StyledBlogContainer>;
+};
+
+export default BlogContainer;
--- easybank-landing-page/src/components/Main.jsx
+++ easybank-landing-page/src/components/Main.jsx
@@ -1,12 +1,14 @@
 import { StyledMain } from "./styles/Main.styled";
 import IntroContainer from "./IntroContainer";
 import FeaturesContainer from "./FeaturesContainer";
+import BlogContainer from "./BlogContainer";
 
 const Main = () => {
   return (
     <StyledMain>
       <IntroContainer />
       <FeaturesContainer />
+      <BlogContainer />
     </StyledMain>
   );
 };
